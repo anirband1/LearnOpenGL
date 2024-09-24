65b08749c8784e1a1bdf2e1518b7daf471ac9752
anirband1 <61051946+anirband1@users.noreply.github.com>
2024-09-24 22:10:48 +0530

    Update cube.h

--- AdvancedOpenGL/dependencies/include/lib/primitives/cube.h
+++ AdvancedOpenGL/dependencies/include/lib/primitives/cube.h
@@ -159,29 +159,6 @@ private:
         -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, //
         -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,  //
     };
-
-    // static void setupVAO()
-    // {
-    //     glGenVertexArrays(1, &VAO);
-
-    //     glGenBuffers(1, &VBO);
-
-    //     // cube
-    //     glBindVertexArray(VAO);
-
-    //     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    //     glBufferData(GL_ARRAY_BUFFER, sizeof(CUBE_PROPERTIES), CUBE_PROPERTIES.data(), GL_STATIC_DRAW);
-
-    //     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0); // position
-    //     glEnableVertexAttribArray(0);
-    //     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float))); // normal
-    //     glEnableVertexAttribArray(1);
-    //     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float))); // texCoord
-    //     glEnableVertexAttribArray(2);
-
-    //     glBindBuffer(GL_ARRAY_BUFFER, 0);
-    //     glBindVertexArray(0);
-    // }
 };
 
 // definition of static
